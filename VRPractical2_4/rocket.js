edc637364b47533bbd4e1633b368663da0a4fdd8
gitkatie23 <katier33@nycstudents.net>
2025-10-28 16:33:17 +0000

    update changes 2_3

--- VRPractical2_4/rocket.js
+++ VRPractical2_4/rocket.js
@@ -2,6 +2,11 @@ class rocket{
   constructor(x,y,z){
     this.obj = document.createElement("a-entity");
   
+    let box = document.createElement("a-box");
+    box.setAttribute("color","red");
+    box.setAttribute("position","0 0 0");
+    box.setAttribute("height","2");
+    this.obj.append( box );
   
     let tube = document.createElement("a-cylinder");
     tube.setAttribute("position","0 0 0");
@@ -14,7 +19,19 @@ class rocket{
     top.setAttribute("position","0 5 0");
     top.setAttribute("height","2");
     this.obj.append( top );
+  }
   
     this.obj.setAttribute("position",{x:x, y:y, z:z});
     scene.append( this.obj )
+
+    launch(){
+    this.y -= this.dy;
+    this.obj.setAttribute("position",{x:this.x, y:this.y, z:this.z})
   }
+
+
+
+
+
+
+}
