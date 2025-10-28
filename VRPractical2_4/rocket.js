0d5a922529858b5917081fbe25b5e9e1cd9a83d9
gitkatie23 <katier33@nycstudents.net>
2025-10-28 16:13:11 +0000

    update changes 2_3

---
+++ VRPractical2_4/rocket.js
@@ -0,0 +1,25 @@
+class rocket{
+  constructor(x,y,z){
+    this.obj = document.createElement("a-entity");
+  
+    let box = document.createElement("a-box");
+    box.setAttribute("color","red");
+    box.setAttribute("position","0 0 0");
+    box.setAttribute("height","1");
+    this.obj.append( box );
+  
+    let tube = document.createElement("a-cylinder");
+    tube.setAttribute("position","0 0 0");
+    tube.setAttribute("color","grey");
+    tube.setAttribute("radius","0.25");
+    this.obj.append( tube );
+
+    let top = document.createElement("a-cone");
+    top.setAttribute("color","red");
+    top.setAttribute("position","0 5 0");
+    top.setAttribute("height","2");
+    this.obj.append( top );
+  
+    this.obj.setAttribute("position",{x:x, y:y, z:z});
+    scene.append( this.obj )
+  }
--- VRPractical2_4/script.js
+++ VRPractical2_4/script.js
@@ -4,12 +4,25 @@ let scene;
 window.addEventListener("DOMContentLoaded",function() {
   scene = document.querySelector("a-scene"); 
 
-
+  box = document.querySelector("a-box");
+  box.a = 0;
+  box.da = 1;
+  
   loop();
 })
 
 function loop(){
+  box.a += box.da;
+  box.setAttribute("rotation",{x:0, y:box.a, z:0});
+  window.requestAnimationFrame(loop);
+}
+
+let rnd = (l,u) => Math.floor(Math.random()*(u-l) + l);
 
   
   window.requestAnimationFrame( loop );
 }
+
+
+
+
