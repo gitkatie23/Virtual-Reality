0327b33abe8b5755e3c941f4c4740d19ba29bc6d
gitkatie23 <katier33@nycstudents.net>
2025-10-29 15:22:32 +0000

    update changes 2_3

--- VRPractical2_4/rocket.js
+++ VRPractical2_4/rocket.js
@@ -1,37 +1,25 @@
 class rocket{
-  constructor(x,y,z){
-    this.obj = document.createElement("a-entity");
-  
-    let box = document.createElement("a-box");
-    box.setAttribute("color","red");
-    box.setAttribute("position","0 0 0");
-    box.setAttribute("height","2");
-    this.obj.append( box );
-  
-    let tube = document.createElement("a-cylinder");
-    tube.setAttribute("position","0 0 0");
-    tube.setAttribute("color","grey");
-    tube.setAttribute("radius","0.25");
-    this.obj.append( tube );
-
-    let top = document.createElement("a-cone");
-    top.setAttribute("color","red");
-    top.setAttribute("position","0 5 0");
-    top.setAttribute("height","2");
-    this.obj.append( top );
-  }
-  
-    this.obj.setAttribute("position",{x:x, y:y, z:z});
-    scene.append( this.obj )
-
-    launch(){
-    this.y -= this.dy;
-    this.obj.setAttribute("position",{x:this.x, y:this.y, z:this.z})
+  constructor(x,z){
+    this.a = 0;
+    this.da = 1;
+    
+    this.obj = document.createElement("a-entity")
+    for(let i = 0; i < 3; i++){
+      let body = document.createElement("a-cylinder");
+      body.setAttribute("position",{x:0,y:0.5 + 1.5 * i,z:0});
+      body.setAttribute("radius",1.25 - 0.20 * i);
+      this.obj.append(body)
+    }
+    for(let i = 0; i < 2; i++){
+      let top = document.createElement("a-cylinder);
+      top.setAttribute("position",{x:-0.25 + 0.50 * i,y:3.5,z:0.85});
+      top.setAttribute("height",2);
+      top.setAttribute("color","yellow")
+      this.obj.append(top)
+    }
+    
+    this.obj.setAttribute("position",{x:x,y:0,z:z});
+    scene.append(this.obj);
   }
 
-
-
-
-
-
-}
+  }
--- VRPractical2_4/script.js
+++ VRPractical2_4/script.js
@@ -1,28 +1,16 @@
 let rnd = (l,u) => Math.floor(Math.random()*(u-l) + l);
-let scene;
+let scene, rocket;
 
 window.addEventListener("DOMContentLoaded",function() {
-  scene = document.querySelector("a-scene"); 
+  scene = document.querySelector("a-scene"); //CSS Selector
 
-  box = document.querySelector("a-box");
-  box.a = 0;
-  box.da = 1;
   
+  snowman = new Snowman(-5,0);
   loop();
 })
 
 function loop(){
-  box.a += box.da;
-  box.setAttribute("rotation",{x:0, y:box.a, z:0});
-  window.requestAnimationFrame(loop);
-}
-
-let rnd = (l,u) => Math.floor(Math.random()*(u-l) + l);
-
-  
+  snowman.spin();
   window.requestAnimationFrame( loop );
 }
-
-
-
 
