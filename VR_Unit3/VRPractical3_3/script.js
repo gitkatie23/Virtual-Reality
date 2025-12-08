c7e95c6b63ef848c1b8e3100d58b7c8df4eeb654
gitkatie23 <katier33@nycstudents.net>
2025-12-08 14:44:39 +0000

    update changes 3_3

--- VR_Unit3/VRPractical3_3/script.js
+++ VR_Unit3/VRPractical3_3/script.js
@@ -4,8 +4,7 @@ let scene, camera, bullet, enemies = [], ammo_boxes = [], ammo_count = 3, enemy_
 window.addEventListener("DOMContentLoaded",function() {
   scene = document.querySelector("a-scene");
   camera = document.querySelector("a-camera");
-   enemy = new Enemy(0,0,-3);
-    loop(); 
+  
   window.addEventListener("keydown",function(e){
     //User can only fire with they press the spacebar and have sufficient ammo
     if(e.key == " " && ammo_count > 0  ){
