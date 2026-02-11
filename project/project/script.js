a706ec60c81a0b2c21835de5e63fb33e6fe05109
gitkatie23 <katier33@nycstudents.net>
2026-02-11 17:32:23 +0000

    Add new textures and styles; remove obsolete script
    - Added new texture files: Default_baseColor.jpeg, Default_metallicRoughness.png, Default_normal.jpeg, and material_0_baseColor.png.
    - Introduced a new CSS file for styling the project, enhancing layout and responsiveness.
    - Removed the outdated script.js file that contained commented-out code.

---
+++ project/project/script.js
@@ -0,0 +1,30 @@
+let rnd = (l,u) => Math.random() * (u-l) + l
+let scene;
+let people = [];
+window.addEventListener("DOMContentLoaded",function() {
+  scene = document.querySelector("a-scene");
+
+  for(let i = 0; i < 40; i++){
+    let x = rnd(-20,20);
+    let z = rnd(-20,20);
+    let person = document.createElement("a-gltf-model");
+    person.setAttribute("src","#animal");
+    person.setAttribute("animation-mixer","");
+    person.setAttribute("position",{x:x,y:0,z:z});
+    person.addEventListener("click",function(){
+      this.setAttribute("scale","0 0 0");
+    })
+    people.push(person);
+    scene.append(person);
+  }
+  
+  loop();
+})
+
+function loop(){
+  for(let person of people){
+    person.object3D.position.z += 0.005;
+  }
+  
+  window.requestAnimationFrame(loop);
+}
